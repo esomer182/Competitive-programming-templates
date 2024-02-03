b9bdb4e9dd923b343391f70159d4f8b31bdd8adb
Alejandro Vivero <96777983+esomer182@users.noreply.github.com>
2024-02-03 12:03:50 +0100

    Update xor_basis.cpp with general log instead of 20.

--- Math/Linear-Algebra/xor_basis.cpp
+++ Math/Linear-Algebra/xor_basis.cpp
@@ -1,16 +1,16 @@
 struct Basis{
-    pair<int, int> basis[20];
+    pair<int, int> basis[LOG];
     vector<int> done;
  
     void init(){
         done.push_back(1);
-        for(int k = 0; k < 20; k++) basis[k] = {-1, -1};
+        for(int k = 0; k < LOG; k++) basis[k] = {-1, -1};
     }
  
     bool add(pair<int, int> p){
         if(done.empty()) init();
         int val = p.first; int j = p.second;
-        for(int k = 19; k >= 0; k--){
+        for(int k = LOG-1; k >= 0; k--){
             if(!((1 << k) & val)) continue;
             if(basis[k].first == -1){
                 basis[k] = {val, j};
